2aff9e3b7b52200bb8b2104170973d9c21c597ab
cchen660 <cchen660@gatech.edu>
2020-09-01 16:55:15 -0400

    code formatting

--- src/components/Auth/login.js
+++ src/components/Auth/login.js
@@ -5,64 +5,64 @@ import { LockOutlined, MailOutlined } from '@ant-design/icons';
 const Login = () => {
     return (
         <Form
-        className="auth"
-        name="normal_login"
-        initialValues={{
-            remember: true,
-        }}
+            className="auth"
+            name="normal_login"
+            initialValues={{
+                remember: true,
+            }}
         >
-        <Form.Item
-            name="email"
-            rules={[
-            {
-                required: true,
-                message: 'Please input your email!',
-            },
-            ]}>
-            <Input
-            autocomplete="email"
-            prefix={<MailOutlined className="site-form-item-icon" />}
-            placeholder="Email"
-            />
-        </Form.Item>
-
-        <Form.Item
-            name="password"
-            rules={[
-            {
-                required: true,
-                message: 'Please input your Password!',
-            },
-            ]}
-            >
-            <Input.Password
-                autocomplete="current-password"
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-            />
-        </Form.Item>
-
-        <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
+            <Form.Item
+                name="email"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your email!',
+                },
+                ]}>
+                <Input
+                autocomplete="email"
+                prefix={<MailOutlined className="site-form-item-icon" />}
+                placeholder="Email"
+                />
             </Form.Item>
 
-            <a className="login-form-forgot" href="">
-            Forgot password
-            </a>
-        </Form.Item>
+            <Form.Item
+                name="password"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your Password!',
+                },
+                ]}
+                >
+                <Input.Password
+                    autocomplete="current-password"
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
 
-        <Form.Item>
-            <Button
-            className="login-form-button"
-            type="primary"
-            htmlType="submit"
-            >
-            Log in
-            </Button>
-        </Form.Item>
-    </Form>
+            <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <a className="login-form-forgot" href="">
+                Forgot password
+                </a>
+            </Form.Item>
+
+            <Form.Item>
+                <Button
+                className="login-form-button"
+                type="primary"
+                htmlType="submit"
+                >
+                Log in
+                </Button>
+            </Form.Item>
+        </Form>
   );
 }
 
