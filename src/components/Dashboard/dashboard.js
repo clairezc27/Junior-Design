816d0c71269e5920d8cc3f2e6e3a5cff14552665
cchen660 <cchen660@gatech.edu>
2020-09-04 01:52:37 -0400

    populate dashboard with searches

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Col, Button, Tooltip } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+import Box from '../Common/box';
 
 const Dashboard = () => {
     return (
@@ -13,11 +14,33 @@ const Dashboard = () => {
                     </Tooltip>
                 </Row>
             </div>
-                 <Button style={{marginBottom: "20px"}} type="primary" size="large">New Search</Button>
+                <Button style={{marginBottom: "20px"}} type="primary" size="large">New Search</Button>
                 <Row>
                     <Col span={8}>Searches</Col>
                     <Col span={8}>Twitter Handle</Col>
                     <Col span={8}>Status</Col>
+                </Row>
+                <Row>
+                    <Col span={8}>
+                        <Box txt="9/1/2020"/>
+                    </Col>
+                    <Col span={8}>
+                        <Box txt="@claire"/>
+                    </Col>
+                    <Col span={8}>
+                        <Box txt="pending"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={8}>
+                        <Box txt="8/27/2020"/>
+                    </Col>
+                    <Col span={8}>
+                        <Box txt="@brett"/>
+                    </Col>
+                    <Col span={8}>
+                        <Box txt="completed 8/31/2020"/>
+                    </Col>
                 </Row>
         </div>
     );
