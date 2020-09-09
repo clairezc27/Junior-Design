ae6c6dec311a1fdeb826fb5f2e34f9232404b169
cchen660 <cchen660@gatech.edu>
2020-09-09 14:50:46 -0400

    connect flask to mysql

--- api/api.py
+++ api/api.py
@@ -1,7 +1,15 @@
 import time
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'MyDB'
+
+mysql = MySQL(app)
 
 @app.route('/time')
 def get_current_time():
