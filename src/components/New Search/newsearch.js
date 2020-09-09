d2dcd7e638aa5afc42ecadebe54194338990aaf4
cchen660 <cchen660@gatech.edu>
2020-09-09 16:31:40 -0400

    fix tabbed nav bar

--- src/components/New Search/newsearch.js
+++ src/components/New Search/newsearch.js
@@ -7,19 +7,22 @@ const NewSearch = () => {
     return (
         <>
         <Navbar  expand="xl" sticky="top" bg="primary" variant="dark" className="justify-content-between">
-            <Navbar.Brand href="#home" style={{ fontSize: "36px"}}>Social Media Mistake Eraser</Navbar.Brand>
+            <Navbar.Brand href="/dashboard" style={{ fontSize: "36px"}}>Social Media Mistake Eraser</Navbar.Brand>
             <Button className="justify-content-end">
                 <IconContext.Provider value={{ style: { fontSize: "20px" } }}>
                     <FaUser />
                 </IconContext.Provider>
             </Button>
         </Navbar>
-        <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav variant="tabs" defaultActiveKey="1">
             <Nav.Item>
-                <Nav.Link href="/home">Step 1</Nav.Link>
+                <Nav.Link eventKey="1">Step 1</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-1">Step 2</Nav.Link>
+                <Nav.Link eventKey="2">Step 2</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="2">Step 2</Nav.Link>
             </Nav.Item>
         </Nav>
         </>
