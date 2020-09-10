c6f97dded50e40852ba2eb8ee0be1eb4e8445994
cchen660 <cchen660@gatech.edu>
2020-09-09 21:43:15 -0400

    add header to home and dashboard

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,37 +1,30 @@
 import React from 'react';
 import { Row, Col, Divider, Button } from 'antd';
 import { Link } from 'react-router-dom';
+import Header from './../Common/header.js'
 
 const Home = () => {
     return (
         <div className="Home">
-            <div className="Home-header">
-                <Row justify="center">
-                    Social Media Mistake Eraser
-                </Row>
-                <Row justify="center" style={{ fontSize: "24px" }}>
-                    Clean up your feed!
-                </Row>
-            </div>
-          <Divider/>
-          <Row>
-            <Col flex={1}>About
-                <br />
-                <p>blah blah blah</p>
-            </Col>
-            <Col style={{ height: "70vh" }}>
-                <Divider type="vertical" style={{height: "100%"}}/>
-            </Col>
-            <Col flex={1}>
-                <Link to="/signup">
-                    <Button type="primary" style={{ marginBottom: "20px" }}>Sign Up</Button>
-                </Link>
-                <br />
-                <Link to="/login">
-                    <Button type="primary" style={{ marginTop: "20px" }}>Login</Button>
-                </Link>
-            </Col>
-          </Row>
+           <Header />
+            <Row>
+                <Col style={{paddingTop: "30px"}} flex={1}>About
+                    <br />
+                    <p>blah blah blah</p>
+                </Col>
+                <Col style={{ height: "70vh" }}>
+                    <Divider type="vertical" style={{height: "100%"}}/>
+                </Col>
+                <Col style={{paddingTop: "30px"}} flex={1}>
+                    <Link to="/signup">
+                        <Button type="primary" style={{ marginBottom: "20px" }}>Sign Up</Button>
+                    </Link>
+                    <br />
+                    <Link to="/login">
+                        <Button type="primary" style={{ marginTop: "20px" }}>Login</Button>
+                    </Link>
+                </Col>
+            </Row>
         </div>
     )
 }
