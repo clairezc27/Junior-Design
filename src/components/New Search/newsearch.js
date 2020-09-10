96d40f5ab8a375bce08ce2f2ab8fc99ff9c4a680
cchen660 <cchen660@gatech.edu>
2020-09-09 21:23:18 -0400

    style fixes

--- src/components/New Search/newsearch.js
+++ src/components/New Search/newsearch.js
@@ -14,6 +14,7 @@ const NewSearch = () => {
                 </IconContext.Provider>
             </Button>
         </Navbar>
+        <h1 className="Home-header">New Search</h1>
         <Nav variant="tabs" defaultActiveKey="1">
             <Nav.Item>
                 <Nav.Link eventKey="1">Step 1</Nav.Link>
@@ -22,7 +23,7 @@ const NewSearch = () => {
                 <Nav.Link eventKey="2">Step 2</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="2">Step 2</Nav.Link>
+                <Nav.Link eventKey="3">Step 3</Nav.Link>
             </Nav.Item>
         </Nav>
         </>
