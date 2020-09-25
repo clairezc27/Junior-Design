6fe471b4479e0fe88fd5912cf57890c774cab53e
clairezc27 <clairezc27@gmail.com>
2020-09-25 11:22:27 -0400

    deployment

---
+++ www/build/precache-manifest.7c40e819e31233a42f51331653848ef1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245e1b126e17765b3398bf532a7b34e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e1af643e6f0e13ea615",
+    "url": "/static/css/2.a0577f7a.chunk.css"
+  },
+  {
+    "revision": "12b8ce1ff22f0f997f08",
+    "url": "/static/css/main.28558f43.chunk.css"
+  },
+  {
+    "revision": "6e1af643e6f0e13ea615",
+    "url": "/static/js/2.8fa3e9cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8fa3e9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12b8ce1ff22f0f997f08",
+    "url": "/static/js/main.874e0877.chunk.js"
+  },
+  {
+    "revision": "4be130620b223c1c9c69",
+    "url": "/static/js/runtime-main.1cbfc7dc.js"
+  }
+]);
