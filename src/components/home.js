b8d19c1ab6ecdb8c5149801d5d9e2b3bb999b601
clairezc27 <clairezc27@gmail.com>
2020-10-08 16:01:16 -0400

    create header

---
+++ src/components/home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Row, Col, Divider } from 'antd';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import Header from './common/header.js'
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <Header />
+      <Col style={{paddingTop: "30px"}} flex={1}>About
+        <br />
+        <p>blah blah blah</p>
+      </Col>
+      <Col style={{ height: "70vh" }}>
+        <Divider type="vertical" style={{height: "100%"}}/>
+      </Col>
+      <Col style={{paddingTop: "30px"}} flex={1}>
+        <Link to="/signup">
+          <Button type="primary" style={{ marginBottom: "20px" }}>Sign Up</Button>
+        </Link>
+        <br />
+        <Link to="/login">
+          <Button type="primary" style={{ marginTop: "20px" }}>Login</Button>
+        </Link>
+      </Col>
+    </div>
+  );
+}
+
+export default Home;
