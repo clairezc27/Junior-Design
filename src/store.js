9b13f68578ce73ba3d4e36db94a20e6e3a04e2e0
clairezc27 <clairezc27@gmail.com>
2020-10-20 20:24:26 -0400

    fixes to redux setup

---
+++ src/store.js
@@ -0,0 +1,34 @@
+
+import { applyMiddleware, createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import storage from 'redux-persist/lib/storage';
+import thunkMiddleware from 'redux-thunk';
+import loggerMiddleware from 'redux-logger';
+
+import rootReducer from './reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default function configureStore(preloadedState) {
+  const middlewares = [loggerMiddleware, thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  const store = createStore(persistedReducer, preloadedState, composedEnhancers);
+
+  const persistor = persistStore(store);
+
+  if (process.env.NODE_ENV !== 'production' && module.hot) {
+    module.hot.accept('./reducers', () => store.replaceReducer(rootReducer));
+  };
+
+  return { persistor, store };
+};
