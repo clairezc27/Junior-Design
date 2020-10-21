b8a072839db46827fd7c63febae5518d25e6e31c
clairezc27 <clairezc27@gmail.com>
2020-10-20 22:13:44 -0400

    deployment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 from flask import Flask, render_template, jsonify, request
+# from google.appengine.api import users
+# from google.auth import app_engine
+# credentials = app_engine.Credentials()
 
 app = Flask(__name__, static_folder='build/', static_url_path='/')
 app.debug = 'DEBUG' in os.environ
@@ -27,3 +30,8 @@ def review():
 # @app.route('/<path:path>')
 # def catch_all(path):
 #   return render_template("index.html")
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=5000, debug=True)
