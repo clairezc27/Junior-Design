014367b0b42a3b0c27e0980fc9210f7ec9b34f4e
clairezc27 <clairezc27@gmail.com>
2020-10-21 22:20:49 -0400

    auth start

--- app.py
+++ app.py
@@ -24,14 +24,8 @@ def store_user():
     # email = request.json['email']
     # username = request.json['username']
     auth = firebase.auth()
-    user = auth.sign_in_with_email_and_password(email, password)
-    db = firebase.database()
-
-    data = {
-        "name": "Joe Tilsed"
-    }
-
-    results = db.child("users").push(data, user['idToken'])
+    auth.create_user_with_email_and_password(email, password)
+    
     return 200
 
 @app.route('/')
