79ef20f73e5870ffc49dbe8513b96b7fb27779a0
clairezc27 <clairezc27@gmail.com>
2020-10-27 18:49:08 -0400

    new search button links to /new-search

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,13 +3,19 @@ import { Row, Col } from 'antd';
 import { Button } from 'react-bootstrap';
 import Box from './common/box.js';
 import Header from './common/header.js'
+import { useHistory } from 'react-router-dom';
 
 const Dashboard = () => {
+  const history = useHistory();
+  const onClick = () => {
+    history.push("/new-search");
+  }
+
   return (
     <div className="Home">
       <Header />
       <h1 className="Home-header">Welcome Home!</h1>
-      <Button className="new-search-btn" type="primary" size="large">New Search</Button>
+      <Button className="new-search-btn" type="primary" size="large" onClick={onClick}>New Search</Button>
       <Row>
         <Col span={8}>Order Date</Col>
         <Col span={8}>Twitter Handle</Col>
