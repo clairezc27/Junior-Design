3ec1772d8914dd785c231a968fd30838c4f45e98
clairezc27 <clairezc27@gmail.com>
2020-10-27 18:57:24 -0400

    delete unused

--- app.py
+++ app.py
@@ -19,35 +19,22 @@ firebase = Firebase(config)
 
 @app.route('/apis/sign-up', methods=['POST'])
 def store_user():
-    print("INSIDE API")
     auth = firebase.auth()    
     auth.create_user_with_email_and_password(request.json['email'], request.json['password'])
-    # user = auth.sign_in_with_email_and_password(request.json['email'], request.json['password'])
-    # db = firebase.database()
     data = {
         'email': request.json['email']
         }   
-    # Pass the user's idToken to the push method
-    # results = db.child("users").push(data, user['idToken'])
     return jsonify(data), 200
 
 @app.route('/apis/login', methods=['POST'])
 def login():
-    # Get a reference to the auth service
     auth = firebase.auth()
-
-    # Log the user in
     user = auth.sign_in_with_email_and_password(request.json['email'], request.json['password'])
     user = auth.refresh(user['refreshToken'])
-    # Get a reference to the database service
     db = firebase.database()
-
-    # data to save
     data = {
         'email': request.json['email']
     }
-
-    # Pass the user's idToken to the push method
     results = db.child("users").push(data, user['idToken'])
     return jsonify(data), 200
 
