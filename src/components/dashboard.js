e877b16dc37b4d53429b575d2a2efbf25ab3c763
clairezc27 <clairezc27@gmail.com>
2020-11-03 18:00:11 -0500

    add dashboard labels back

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Row, Col } from 'antd';
 import Box from './common/box.js';
 import Header from './common/header.js'
 import { useHistory } from 'react-router-dom';
@@ -17,6 +18,11 @@ const Dashboard = () => {
       <Header />
       <h1 className="Home-header">Welcome Home!</h1>
       <Button className="new-search-btn" type="primary" size="large" onClick={onClick}>New Search</Button>
+      <Row>
+        <Col span={8}>Order Date</Col>
+        <Col span={8}>Twitter Handle</Col>
+        <Col span={8}>Status</Col>
+      </Row>
       <Box />
     </div>
   );
