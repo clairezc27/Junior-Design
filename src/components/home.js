9c82a4e076911d8d4501c35e64bc52b3ea9ee5f6
rwackes3 <rwackes3@gatech.edu>
2020-11-13 13:51:13 -0500

    Update to reflect better language around word list

--- src/components/home.js
+++ src/components/home.js
@@ -21,7 +21,7 @@ const Home = () => {
           <p>Our goal, as members of Generation Z, is to create and promote a safe space for everyone on social media platforms starting with Twitter - especially since more people are growing up with social media. Whether you have a big job interview coming up, you recently came into fame, you are looking to help a friend or client, or you are just trying to be an upstanding citizen, the Social Media Mistake Flagger can help.</p>
           <br />
           <h3>How it Works</h3>
-          <p>To receive a report of what tweets are flagged from our undisputed list of “bad” words from your Twitter account create an account and set up a new search by inputting your Twitter handle and any additional words you would like our product to search for. From there, we will use our list and any additional words you provided to make a comprehensive list of tweets that we suggest you delete from your account to make sure your Twitter account is promoting a safe space as well. Yes, it’s that easy, and it’s completely free.</p>
+          <p>To receive a report of what tweets are flagged from our undisputed list of potentially inappropriate words from your Twitter account, create an account and set up a new search by inputting your Twitter handle and any additional words you would like our product to search for. From there, we will use our hand-crafted list and any additional words you provided to return a comprehensive list of tweets that we suggest you delete from your account to make sure your Twitter account is promoting a safe space as well. Yes, it’s that easy, and it’s completely free.</p>
           </div>
           </Col>
         <Col style={{ height: "80vh" }}>
