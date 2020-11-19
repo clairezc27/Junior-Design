28965ddc8e99812044b4dcbe34837ded3891d3fb
rwackes3 <rwackes3@gatech.edu>
2020-11-18 23:37:53 -0500

    Add error message when user tries to reset password but password mismatches and fix error styling.

--- src/components/account.js
+++ src/components/account.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import { Row, Col } from 'antd';
 import { Button } from 'react-bootstrap';
 import Header from './common/header.js'
@@ -15,20 +16,37 @@ const Account = () => {
   const currUser = useSelector(state => state.auth.currUser);
   const dispatch = useDispatch();
   const history = useHistory();
-  let pwd = "";
+  // state hooks
+  const [password, setNewPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  
+  // set the value of password
+  const newPasswordFinish = (event) => {
+    setNewPassword(event.target.value);
+  }
+  
+  // set the value of confirmPassword
+  const confirmPasswordFinish = (event) => {
+    setConfirmPassword(event.target.value)
+  }
 
+  // upon clicking delete account button, dispatch and start delete account process
   const deleteClicked = () => {
     dispatch(deleteUser(currUser));
     dispatch(clearStore());
     history.push("/");
   }
 
-  const pwdChange = (event) => {
-    pwd = event.target.value;
-  }
-
+  // upon clicking reset password button, dispatch and start reset process
   const resetClicked = () => {
-    dispatch(resetUser(currUser, pwd));
+    if (password != confirmPassword) {
+      // if passwords mismatch, display appropriate error message
+      const errorElement = <p>The passwords you entered did not match.</p>
+      ReactDOM.render(errorElement, document.getElementsByClassName('reset-error')[0]);
+    } else {
+      // if passwords match, dispatch and start reset process
+      dispatch(resetUser(currUser, password));
+    }
   }
 
   return (
@@ -53,8 +71,9 @@ const Account = () => {
 					<br />
 					<InputGroup className="mb-3">
 						<Form.Group>
-							<Form.Control type="password" placeholder="New password" onChange={pwdChange}/> 
-							<Form.Control type="password" placeholder="Confirm password" />
+							<Form.Control type="password" placeholder="New Password" onChange={newPasswordFinish}/> 
+							<Form.Control type="password" placeholder="Confirm New Password" onChange={confirmPasswordFinish}/>
+              <div className="reset-error"></div>
 							<br />
 							<Button type="primary" onClick={resetClicked}>Submit</Button>
 						</Form.Group>
